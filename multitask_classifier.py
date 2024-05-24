20d5eb80624cf7b6f97d6bfdc2433264f3cb7772
oliverlee000 <oliver.lee@stanford.edu>
2024-05-23 18:48:29 -0700

    generic multitask code

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -24,8 +24,6 @@ from bert import BertModel
 from optimizer import AdamW
 from tqdm import tqdm
 
-from classifier import BertSentimentClassifier
-
 from datasets import (
     SentenceClassificationDataset,
     SentenceClassificationTestDataset,
@@ -53,6 +51,7 @@ def seed_everything(seed=11711):
 
 BERT_HIDDEN_SIZE = 768
 N_SENTIMENT_CLASSES = 5
+DROPOUT_PROB = 0.1
 
 
 class MultitaskBERT(nn.Module):
@@ -74,19 +73,28 @@ class MultitaskBERT(nn.Module):
             elif config.fine_tune_mode == 'full-model':
                 param.requires_grad = True
         # You will want to add layers here to perform the downstream tasks.
+        # Layers for sentiment
+        self.sentiment_dropout = nn.Dropout(DROPOUT_PROB)
+        self.sentiment_dense = nn.Linear(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES)
 
-        self.sentiment_dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.sentiment_dense = nn.Linear(config.hidden_size, config.num_labels)
+        # Layers for paraphrase
+        self.paraphrase_dropout = nn.Dropout(DROPOUT_PROB)
+        self.paraphrase_dense = nn.Linear(2*BERT_HIDDEN_SIZE, 1)
+
+        # Layers for similarity
+        self.similarity_dropout = nn.Dropout(DROPOUT_PROB)
+        self.similarity_dense = nn.Linear(2*BERT_HIDDEN_SIZE, 1)
 
     def forward(self, input_ids, attention_mask):
         'Takes a batch of sentences and produces embeddings for them.'
-        model_generated = self.bert(input_ids, attention_mask)
-        return model_generated[1]
+
         # The final BERT embedding is the hidden state of [CLS] token (the first token)
         # Here, you can start by just returning the embeddings straight from BERT.
         # When thinking of improvements, you can later try modifying this
         # (e.g., by adding other layers).
         ### TODO
+        output = self.bert(input_ids, attention_mask)
+        return output['last_hidden_state']
 
     def predict_sentiment(self, input_ids, attention_mask):
         '''Given a batch of sentences, outputs logits for classifying sentiment.
@@ -108,22 +116,27 @@ class MultitaskBERT(nn.Module):
         Note that your output should be unnormalized (a logit); it will be passed to the sigmoid function
         during evaluation.
         '''
-        bertOutput1 = self.forward(input_ids_1, attention_mask_1)
-        bertOutput2 = self.forward(input_ids_2, attention_mask_2)
-        concat = torch.cat((bertOutput1, bertOutput2), 1)
-        return (self.predict_paraphrase(concat))
+        output1 = self.bert(input_ids_1, attention_mask_1)
+        output2 = self.bert(input_ids_2, attention_mask_2)
+        output_agr = torch.cat((output1['pooler_output'], output2['pooler_output']), 1)
+        output_agr = self.paraphrase_dropout(output_agr)
+        output_agr = self.paraphrase_dense(output_agr)
+        return output_agr
 
 
     def predict_similarity(self,
                            input_ids_1, attention_mask_1,
                            input_ids_2, attention_mask_2):
-        bertOutput1 = self.forward(input_ids_1, attention_mask_1)
-        bertOutput2 = self.forward(input_ids_2, attention_mask_2)
-        concat = torch.cat((bertOutput1, bertOutput2), 1)
-        return (self.semantic_simarlity(concat))
         '''Given a batch of pairs of sentences, outputs a single logit corresponding to how similar they are.
         Note that your output should be unnormalized (a logit).
         '''
+
+        output1 = self.bert(input_ids_1, attention_mask_1)
+        output2 = self.bert(input_ids_2, attention_mask_2)
+        output_agr = torch.cat((output1['pooler_output'], output2['pooler_output']), 1)
+        output_agr = self.similarity_dropout(output_agr)
+        output_agr = self.similarity_dense(output_agr)
+        return output_agr
 
 
 
