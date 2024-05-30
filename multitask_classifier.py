f78f12cc0da741edc85326cf2b0d49329b9c832d
oliverlee000 <oliver.lee@stanford.edu>
2024-05-29 22:45:51 -0700

    fix

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -129,8 +129,8 @@ class MultitaskBERT(nn.Module):
         
         if config.num_sts_layers > 1:
             self.sts_layers = nn.ModuleList([FF(2*BERT_HIDDEN_SIZE, LINEAR_LAYER_HIDDEN_SIZE)])
-            self.sts_layers.extend([FF(2*LINEAR_LAYER_HIDDEN_SIZE, 2*LINEAR_LAYER_HIDDEN_SIZE) for _ in range(config.num_sts_layers - 2)])
-            self.sts_layers.append(FF(2*LINEAR_LAYER_HIDDEN_SIZE, 1))
+            self.sts_layers.extend([FF(LINEAR_LAYER_HIDDEN_SIZE, LINEAR_LAYER_HIDDEN_SIZE) for _ in range(config.num_sts_layers - 2)])
+            self.sts_layers.append(FF(LINEAR_LAYER_HIDDEN_SIZE, 1))
         else:
             self.sts_layers = nn.ModuleList([FF(2*BERT_HIDDEN_SIZE, 1)])
 
