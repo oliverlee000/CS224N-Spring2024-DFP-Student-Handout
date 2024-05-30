4e81c811863bef596f26215d4a0a92cb0b69b5a7
oliverlee000 <oliver.lee@stanford.edu>
2024-05-29 21:43:34 -0700

    relu begone

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -115,7 +115,7 @@ class MultitaskBERT(nn.Module):
         self.sst_layers.append(FF(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES))
         self.para_layers = nn.ModuleList([FF(2*BERT_HIDDEN_SIZE, 2*BERT_HIDDEN_SIZE) for _ in range(config.num_para_layers - 1)])
         self.para_layers.append(FF(2*BERT_HIDDEN_SIZE, 1))
-        self.sts_layers = nn.ModuleList([FF(2*BERT_HIDDEN_SIZE, 2*BERT_HIDDEN_SIZE, relu=True) for _ in range(config.num_sts_layers - 1)])
+        self.sts_layers = nn.ModuleList([FF(2*BERT_HIDDEN_SIZE, 2*BERT_HIDDEN_SIZE) for _ in range(config.num_sts_layers - 1)])
         self.sts_layers.append(FF(2*BERT_HIDDEN_SIZE, 1))
 
     def forward(self, input_ids, attention_mask):
