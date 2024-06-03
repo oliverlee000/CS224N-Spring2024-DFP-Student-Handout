72c25e1a60acf2a3b8737036c26c6aaf8e1b03d1
manatk <manatmadan@gmail.com>
2024-06-02 20:21:57 -0700

    fixed bug

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -240,8 +240,8 @@ def train_multitask(args):
 
     # Set loss function for sts
     sts_loss_fn = lambda sts_logits, sts_labels: F.mse_loss(sts_logits, sts_labels, reduction='sum') / args.batch_size
-    if args.pearson_loss == 'y':
-        sts_loss_fn = pearson_coefficient_loss
+    # if args.pearson_loss == 'y':
+    #     sts_loss_fn = pearson_coefficient_loss
 
     # Run extra fine tuning loss functions for bert embeddings:
     if args.cos_sim_loss == 'y' or args.neg_rankings_loss == 'y':
